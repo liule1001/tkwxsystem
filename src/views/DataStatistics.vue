59c7771fa2f8261953d25072e5174e45c3d17c45
lanfang233 <53343545+lanfang233@users.noreply.github.com>
2020-07-01 17:45:29 +0800

    数据统计button按钮

--- src/views/DataStatistics.vue
+++ src/views/DataStatistics.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
+    <div class="elRow">
+      <div class="elDate">
+        <el-button  size="small" >当日</el-button>
+        <el-button  size="small" >昨日</el-button>
+        <el-button  size="small" >本周</el-button>
+        <el-button  size="small" >本月</el-button>
+      </div>
+      <div class="elTime">
+        <el-button  size="small" >自定义时段</el-button>
+        <el-button  size="small" disabled>起始日期</el-button>
+        <span>到</span>
+        <el-button  size="small" disabled>终止日期</el-button>
+        <el-button  size="small" >查询</el-button>
+      </div>
+      <el-button  size="small" >导出Excle</el-button>
+    </div>
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="date" label="日期" width="150"></el-table-column>
       <el-table-column label="地址">
@@ -22,5 +38,14 @@ export default {
   methods: {}
 };
 </script>
-<style lang="scss" rel="stylesheet/scss">
+<style lang="scss" rel="stylesheet/scss" scoped>
+.el-button:focus, .el-button:hover{
+  color: #14b5b0 !important;
+}
+.elRow {
+  display: flex;
+  .elDate{
+    margin-right: 20px;
+  }
+}
 </style>
