caccec9f18ad790a022032a683df5504a92668eb
liule1001 <1762219852@qq.com>
2020-07-01 17:02:48 +0800

    敏感

--- src/views/DataStatistics.vue
+++ src/views/DataStatistics.vue
@@ -1,82 +1,43 @@
 <template>
     <div class="about">
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
-        <h1>仪表盘页面，没有导航，之后放一个按钮可以返回home页，现在点返回按钮返回</h1>
+       <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="150">
+    </el-table-column>
+    <el-table-column label="配送信息">
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120">
+      </el-table-column>
+      <el-table-column label="地址">
+        <el-table-column
+          prop="province"
+          label="省份"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="city"
+          label="市区"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+        </el-table-column>
+        <el-table-column
+          prop="zip"
+          label="邮编"
+          width="120">
+        </el-table-column>
+      </el-table-column>
+    </el-table-column>
+  </el-table>
     </div>
 </template>
 
@@ -91,103 +52,10 @@ export default {
         };
     },
     mounted() {
-        //防抖
-        // this.timeout(100).then((value) => {
-        //     console.log(value);
-        // });
-        // window.addEventListener('scroll', this.debounce(this.handle, 2000));
-        // window.addEventListener("scroll", this.throttle(this.handle, 2000));
-        // const p1 = new Promise(function(resolve, reject) {
-        //     console.log('resolve',resolve);
-        //     console.log('reject',reject);
-        //     setTimeout(() => reject(new Error("fail")), 3000);
-        // });
-
-        // const p2 = new Promise(function(resolve) {
-        //     console.log('resolve',resolve);
-        //     setTimeout(() => resolve(p1), 1000);
-        // });
-
-        // p2.then(result => console.log(result)).catch(error =>
-        //     console.log(error)
-        // );
-        // // Error: fail
-        // var p1 = new Promise((resolve) => {
-        //     return resolve(1);
-        // });
-        const p = Promise.resolve('Hello');
-        console.log(p);
+      
     },
     methods: {
-        timeout(ms) {
-            return new Promise(resolve => {
-                setTimeout(resolve, ms, "done");
-            });
-        },
-        // 防抖
-        // debounce(fn, wait){
-        //     var timeout = null;      //定义一个定时器
-        //     console.log('我又执行了')
-        //     return function() {
-        //         if(timeout !== null) {
-        //             console.log('清除定时器')
-        //             clearTimeout(timeout);  //清除这个定时器
-        //         }
-        //         timeout = setTimeout(fn, wait);
-        //     }
-        // },
-        // handle() {
-        //     console.log(Math.random());
-        // },
-
-        // throttle(func, delay) {
-        //     var prev = Date.now();
-        //     return function() {
-        //         // var context = this; //this指向window
-        //         var args = arguments;
-        //         console.log('args',args)
-        //         var now = Date.now();
-        //         if (now - prev >= delay) {
-        //             // func.apply(context, args);
-        //             func();
-        //             prev = Date.now();
-        //         }
-        //     };
-        // }
-        // throttle(func, delay) {
-        //     var timer = null;
-        //     return function() {
-        //         var context = this;
-        //         var args = arguments;
-        //         if (!timer) {
-        //             timer = setTimeout(function() {
-        //                 func.apply(context, args);
-        //                 timer = null;
-        //             }, delay);
-        //         }
-        //     };
-        // }
-        throttle(func, delay) {
-            var timer = null;
-            var startTime = Date.now(); //设置开始时间
-            return function() {
-                var curTime = Date.now();
-                var remaining = delay - (curTime - startTime); //剩余时间
-                var context = this;
-                var args = arguments;
-                clearTimeout(timer);
-                if (remaining <= 0) {
-                    // 第一次触发立即执行
-                    console.log("剩余时间小于0");
-                    func.apply(context, args);
-                    startTime = Date.now();
-                } else {
-                    console.log("剩余时间大于0");
-                    console.log("remaining", remaining);
-                    timer = setTimeout(func, remaining); //取消当前计数器并计算新的remaining
-                }
-            };
-        }
+      
     }
 };
 </script>
--- src/views/SensitiveWordsManagement.vue
+++ src/views/SensitiveWordsManagement.vue
@@ -1,10 +1,73 @@
 <template>
   <div class="about">
     <h1>敏感词管理</h1>
+    <el-button plain @click="add">新增敏感词</el-button>
+    <div>已有敏感词</div>
+    <div v-for="(item,index) of dataSource" v-bind:key="index">
+      <div>
+        <el-button plain>{{item}}</el-button>
+        <div :class="edit_btn">
+          <div @click="cancle">
+            <i class="el-icon-close"></i>
+          </div>
+          <div @click="edit">
+            <i class="el-icon-edit"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      popTitle: "",
+      dataSource: ["1", "2", "3"]
+    };
+  },
+  methods: {
+    open() {
+      this.$prompt(this.popTitle, {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        // inputPattern: ,
+        inputErrorMessage: "长度最多16个字符(1个汉字=2个字符)"
+      })
+        .then(() => {
+          // 接口请求
 
-
+          this.$message({
+            type: "success",
+            message: this.popTitle + "成功"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "取消输入"
+          });
+        });
+    },
+    cancle() {
+       this.$message({
+            type: "info",
+            message: "删除成功"
+          });
+    },
+    edit() {
+      this.popTitle = "修改敏感词";
+      this.open();
+    },
+    add() {
+      this.popTitle = "新增敏感词";
+      this.open();
+    }
+  }
+};
+</script>
 <style lang="scss" rel="stylesheet/scss">
-
+.edit_btn{
+  display: flex;
+}
 </style>
