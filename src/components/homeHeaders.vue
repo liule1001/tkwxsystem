ec7cd36e11f03b5a096bc2ac901f06506f683239
liule1001 <1762219852@qq.com>
2020-07-01 17:18:32 +0800

    test

---
+++ src/components/homeHeaders.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <!-- <div class="loginBox">
+            <span>欢迎您，</span>
+            <span>王五</span>
+            <span class="logout">退出</span>
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // childName: this.name,
+        };
+    },
+    props:{
+        // name:{
+        //     type: String,
+        //     required: false,
+        //     default:"李四"
+        // }
+    },
+    components:{
+    },
+    methods: {
+        
+
+    },
+    mounted() {
+
+    },
+};
+</script>
+
+<style lang='scss' rel='stylesheet/scss'>
+@import "../assets/scss/common.scss";
+// @import "../assets/scss/config.scss";
+.el-header {
+    height: 70px !important;
+    background: #252a2f;
+    color: #fff
+}
+.loginBox {
+    float: right;
+    .logout {
+        margin-left: 20px;
+        cursor: pointer;
+        color: #fff;
+    }
+}
+</style>
