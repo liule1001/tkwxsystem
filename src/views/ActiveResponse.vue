cc822c6dad0f6e324d369580552089f7cb9ad01e
lanfang233 <53343545+lanfang233@users.noreply.github.com>
2020-07-02 14:59:13 +0800

    主被动响应

--- src/views/ActiveResponse.vue
+++ src/views/ActiveResponse.vue
@@ -1,10 +1,57 @@
 <template>
   <div class="about">
-    <h1>参数管理页面</h1>
+    <el-col   class="staffList">
+            <h5>员工姓名</h5>
+            <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#ddd"
+            text-color="#333"
+            active-text-color="#333"
+        >
+        </el-menu>
+        </el-col>
+        <el-col class="sessionList">
+            <h5>客户备注</h5>
+            <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#ddd"
+            text-color="#333"
+            active-text-color="#333"
+        >
+            <el-menu-item>
+                <Session/>
+            </el-menu-item>
+
+            <el-menu-item>
+                 <Session/>
+            </el-menu-item>
+        </el-menu>
+        </el-col>
+        <el-col>
+            <h5>聊天记录</h5>
+            <Chat />
+        </el-col>
   </div>
 </template>
 
 
 <style lang="scss" rel="stylesheet/scss">
-
+@import "../assets/scss/sessionBack.scss";
+.staffList, .sessionList{
+    .el-submenu__title {
+        border-bottom: 1px solid #ccc;
+    }
+     .el-menu-item {
+        padding: 0 15px !important;
+        border-bottom: 1px solid #ccc;
+        * {
+            vertical-align: top;
+        }
+    }
+}
+.sessionList .el-menu-item{
+    height: 60px;
+}
 </style>
