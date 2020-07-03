6e58c8903a911628f541be30f5b70d1cfb990179
lanfang233 <53343545+lanfang233@users.noreply.github.com>
2020-07-03 10:27:50 +0800

    被动响应

--- src/views/PassiveResponse.vue
+++ src/views/PassiveResponse.vue
@@ -1,74 +1,201 @@
 <template>
-    <div class="responseBox">
-        <div class="picBox">
-            <img src="1231" alt="" class="staffPic">
-            
-        </div>
-        <div class="center">
-            <div class="name">
-                弗洛伊德
-            </div>
-            <div class="message ellipsis">
-               <span class="count">18</span>
-               主动响应
-            </div>
-        </div>
+  <div class="activeResponse">
+      <h3>被动响应客户</h3>
+    <div style="margin: 5px 0 20px 0;">
+      <el-radio-group v-model="radio1" size="small">
+        <el-radio label="1" border>当日</el-radio>
+        <el-radio label="2" border>昨日</el-radio>
+        <el-radio label="3" border>本周</el-radio>
+        <el-radio label="4" border>本月</el-radio>
+      </el-radio-group>
     </div>
+    <div>
+      <el-col class="staffList">
+        <h5>员工列表</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#ddd"
+          text-color="#333"
+          active-text-color="#333"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <span>敏感词</span>
+            </template>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <span>客户响应</span>
+            </template>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+            <el-menu-item>
+              <Response />
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-col>
+      <el-col class="sessionList">
+        <h5>会话列表</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#ddd"
+          text-color="#333"
+          active-text-color="#333"
+        >         
+         <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+          <el-menu-item>
+            <CustomerNotes />
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col>
+        <h5>聊天记录</h5>
+        <Chat />
+      </el-col>
+    </div>
+  </div>
 </template>
 
 <script>
+import Response from "@/components/Response.vue";
+import Chat from "@/components/Chat.vue";
+import CustomerNotes from "@/components/CustomerNotes.vue";
 export default {
-    data() {
-        return {
-            
-        };
-    },
-    methods: {
-       
-    },
-    mounted() {
-       
-    },
+  name: "hello",
+  data() {
+    return {
+      radio1: "1"
+    };
+  },
+  components: {
+    Chat,
+    Response,
+    CustomerNotes
+  },
+  mounted() {},
+  methods: {}
 };
 </script>
 
-<style lang='scss' rel='stylesheet/scss'>
-@import "../assets/scss/common.scss";
-.responseBox {
-    width: 100%;
-    height: 60px;
-    padding: 0 0 0 30px;
-    display: flex;
-    .picBox {
-        width: 70px;
-        .staffPic {
-            display: inline-block;
-            border-radius: 50%;
-            height: 34px;
-            width: 34px;
-            background: #a2e5ec;
-            margin: 14px 10px 0 0;
-        }
-    }
-    .center {
-       width: calc(100% - 120px);
-        .name {
-            height: 32px;
-            line-height: 40px;
-            font-size: 14px;
-            font-weight: 700;
-            overflow: hidden ;
-        }
-        .message {
-            height: 20px;
-            line-height: 20px;
-            font-size: 12px;
-            .count {
-                font-weight: 700;
-            }
-        }
-    }
+<style lang="scss" rel="stylesheet/scss" >
+@import "../assets/scss/sessionBack.scss";
+.activeResponse .el-col {
+  height: calc(100vh - 204px);
 }
-
-
+.activeResponse .el-menu-vertical-demo {
+  height: auto;
+  max-height: calc(100vh - 255px);
+  overflow-y: auto;
+}
+.staffList,
+.sessionList {
+  .el-submenu__title {
+    border-bottom: 1px solid #ccc;
+  }
+  .el-menu-item {
+    padding: 0 15px !important;
+    height: 60px;
+    border-bottom: 1px solid #ccc;
+    * {
+      vertical-align: top;
+    }
+  }
+}
+.activeResponse .sessionList .el-menu-item {
+  height: 60px;
+}
 </style>
