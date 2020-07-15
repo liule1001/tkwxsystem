d4a7323b3b642e0ae63913d53af5f6e28b72f027
yuanyaquan <yuanyaquan@cnpc.com.cn>
2020-07-15 16:08:53 +0800

    格式化文件

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,21 @@ module.exports = {
     runtimeCompiler: true,
     publicPath: '/', // 设置打包文件相对路径
     devServer: {
-      // open: process.platform === 'darwin',
-      // host: 'localhost',
-      port: 8071,
-      // open: true, //配置自动启动浏览器 
-      proxy: {
-        '/api': {
-          target: 'http://139.9.138.74:8080/', //对应自己的接口
-          changeOrigin: true,
-          ws: true,
-          pathRewrite: {
-            '^/api': ''
-          }
+        // open: process.platform === 'darwin',
+        // host: 'localhost',
+        port: 8071,
+        // open: true, //配置自动启动浏览器 
+        proxy: {
+            //遇到这种带api字符的会主动代理到后台
+            '/api': {
+                target: 'http://139.9.138.74:8080/', //对应自己的接口
+                changeOrigin: true,
+                ws: true,
+                // 把api开头的路径用''代替/api,以访问到后端接口
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
         }
-      }
-     }, 
-  }
+    },
+}
